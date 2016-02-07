3990bf7696b79233ff68adb8249dc2e0e11ebe7b
Nick Stevens <nick0stevens@gmail.com>
2016-02-07 17:34:26 +0000

    Initial Commit

---
+++ another-git-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  another-git-test
+//
+//  Created by Nick Stevens on 07/02/2016.
+//  Copyright © 2016 Nick Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
